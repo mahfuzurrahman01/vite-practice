3e0b86b28c41f656c868adfddc19945bb1679419
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-06 12:24:31 +0600

    adding the contact.jsx,edi.jxs file and make some changes

--- src/main.jsx
+++ src/main.jsx
@@ -10,18 +10,39 @@
 // )
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
 } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage></ErrorPage>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+      },
+    ]
   },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/routes/contact.jsx
@@ -0,0 +1,97 @@
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contact";
+
+
+export async function loader({ params }) {
+    return getContact(params.contactId);
+  }
+export default function Contact() {
+    const contact = useLoaderData()
+//   const contact = {
+//     first: "locas",
+//     last: "Morgan",
+//     avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+//     twitter: "your_handle",
+//     notes: "You don’t have to always be strong. Let the tears flow when they need to. Give yourself time for everything to unfold.",
+//     favorite: true,
+return (
+    
+    <div id="contact">
+      <div>
+        <img
+          key={contact.avatar}
+          src={contact.avatar || null}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter && (
+          <p>
+            <a
+              target="_blank"
+              href={`https://twitter.com/${contact.twitter}`}
+            >
+              {contact.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Please confirm you want to delete this record."
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+  };
+
+
+
+function Favorite({ contact }) {
+  // yes, this is a `let` for later
+  let favorite = contact.favorite;
+  return (
+    <Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={
+          favorite
+            ? "Remove from favorites"
+            : "Add to favorites"
+        }
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+}
